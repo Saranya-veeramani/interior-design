bcc1c5065eb95d4a035d79bf9f18d10e603dddfe
Saranya-veeramani <120944072+Saranya-veeramani@users.noreply.github.com>
2023-03-10 19:39:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Services from './pages/Services'
+import Footer from './components/Footer'
+import Contact from './pages/Contact'
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+              <Route path="/Services"element={<Services/>}/>
+        <Route path="/Contact"element={<Contact/>}/>
+      </Routes>
+       <Footer/>       </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
